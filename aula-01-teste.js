63cd14f7c509692f674690dac43e213ef5a5aaec
Claudio Lupi TI <156871021+claudiolupiti@users.noreply.github.com>
2024-10-21 23:01:54 -0300

    Add files via upload

---
+++ aula-01-teste.js
@@ -0,0 +1,49 @@
+﻿/*
+let nome = "Claudio";
+const idade = 25;
+
+alert("Olá, "+nome+" Você tem "+idade+" anos.");
+
+function saudacao(nome){
+    alert("Olá, "+nome+"!");
+}
+
+saudacao("Seja bem-vindo")
+
+var num1 = 1;
+var num2 = 2;
+
+var soma = num1 + num2;
+
+console.log(soma);
+
+
+var num1 = 5;
+var num2 = 10;
+
+var soma = num1 + num2;
+var subtracao = num2 - num1;
+var multiplicacao = num1 * num2;
+var divisao = num2 / num1;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
+var username = "ClaudioLupi";
+var password = "Teste123";
+
+if(username == "ClaudioLupi" && password == "Teste123"){
+    console.log("Logado com sucesso !");
+}else{
+    console.log("Senha ou usuário errados !");
+}
+
+*/
+
+var a = 5;
+
+while(a > 10){
+    console.log("Boa noite !!!");
+}
